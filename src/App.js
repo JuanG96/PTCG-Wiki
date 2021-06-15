9ae5c48ecec859e4b7ecb2178d805335ebd8ffb7
Juan <juangianina96@gmail.com>
2021-06-15 10:34:23 +0200

    API, sass, react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import logo from './logo.svg';
-import './App.css';
+import './assets/scss/App.scss';
+import React, { useEffect, useState } from 'react'
 
 function App() {
+  let API = "https://api.pokemontcg.io/v2/cards"
+
+  let [asd, setAsd ] = useState()
+
+  useEffect(() => {
+
+    fetch(API, {
+      method: 'GET',
+      headers: {
+        'x-api-key' : '078f5182-ba40-4b8c-8946-788acbb1684b'
+      }
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+  },[])
+    
   return (
     <div className="App">
       <header className="App-header">
